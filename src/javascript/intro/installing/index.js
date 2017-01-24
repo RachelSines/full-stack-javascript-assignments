99fd9b23b3dbe113efb1bfd928b5888b61bddaeb
Unknown <r8chel.signs@gmail.com>
2017-01-24 09:15:36 -0700

    Personal Info

--- src/javascript/intro/installing/index.js
+++ src/javascript/intro/installing/index.js
@@ -1,8 +1,13 @@
-export const name = '';
+export const name = 'Rachel Sines';
 
-export const email = '';
+export const email = 'r8chel.signs@gmail.com';
 
+export const phone = '801-739-4301'
 
 // Write code here
+
+console.log(name);
+console.log(email);
+console.log(phone);
 
 
